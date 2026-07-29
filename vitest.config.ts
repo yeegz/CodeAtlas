31109db3f093ac96b6df545096975e1784fb1c9b
Yousof Selim <165985093+yeegz@users.noreply.github.com>
2026-07-29 19:49:49 +0800

    test: exclude installed workspace links

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: ["**/fixtures/**/test/**"],
+    exclude: ["**/fixtures/**/test/**", "**/node_modules/**"],
     include: ["**/test/**/*.test.ts"],
     passWithNoTests: true,
   },
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,7 +3,7 @@ import { defineWorkspace } from "vitest/config";
 export default defineWorkspace([
   {
     test: {
-      exclude: ["**/fixtures/**/test/**"],
+      exclude: ["**/fixtures/**/test/**", "**/node_modules/**"],
       include: ["**/test/**/*.test.ts"],
     },
   },
